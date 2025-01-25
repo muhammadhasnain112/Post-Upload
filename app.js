ee90bdb284d378fcb2490688a3cd9bbf02152ff4
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2025-01-25 18:13:19 +0500

    Post Upload

--- app.js
+++ app.js
@@ -8,32 +8,31 @@ let fbtn = document.getElementById("firstbtn").addEventListener('click', functio
     let sbtn = document.getElementById('click')
     // let abc = document.getElementById('picture')
     sbtn.addEventListener('click', function () {
+        let date = new Date().toLocaleDateString();
+        let time = new Date().toLocaleTimeString();
         let file = pic.files[0]
         let url = URL.createObjectURL(file)
-
+        let count = 0;
         sdiv.innerHTML += `<div id="innerdiv">
         <h1>${input.value}</h1>
         <img src="${url}" alt="" id="picture">
-        <p><b>Description:</b>${textarea.value}</p>
+        <p><b>Description : </b>${textarea.value}</p>
         <div id="img">
-        <img src="1.png" alt="" height="30" onclick="foo()" id="heart"><img src="comment.png" alt="" height="30" id="comment"><img
+        <img src="1.png" alt="" height="30" id="heart${count}"><img src="comment.png" alt="" height="30" id="comment"><img
         src="share-arrow.png" alt="" height="30">
         </div>
+        <p>${time} ${date}</p>
         </div>`
         // console.log(file);
         fdiv.innerHTML = ``
-        let heart = document.getElementById('heart')
+        let heart = document.getElementById(`heart${count}`)
         heart.addEventListener('click', function () {
-            // function foo(){
-
+            ++count
             heart.src = `2.png`
-            // }    
         })
-        
+
         document.getElementById('comment').addEventListener('click', function () {
-            fdiv.innerHTML = `        <textarea name="" id="abcd" placeholder="Enter Your Comment Here"></textarea>
-<button id="com">Send</button>
-`
+            fdiv.innerHTML = `<textarea name="" id="abcd" placeholder="Enter Your Comment Here"></textarea><button id="com">Send</button>`
             document.getElementById('com').addEventListener('click', function () {
                 fdiv.innerHTML = ``
                 alert("Thanks For Comment")
